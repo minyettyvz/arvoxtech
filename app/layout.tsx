b79c983d4f38b88c22452daae381cc6d41cf21bd
Alvaro Minyety <joaquinminyetty@gmail.com>
2025-08-05 09:56:55 -0400

    2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Arvox Tech',
+  description: 'Arvox Tech',
+  generator: 'Arvox Tech',
 }
 
 export default function RootLayout({
